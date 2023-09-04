9dcbea3ef4d7cf9ab1b5e7e6558d7e884b7a7abc
Ahmed Ibrahim <60679729+AhmedIbrahimai@users.noreply.github.com>
2023-09-04 20:49:36 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,31 @@
+import cv2
+
+
+  
+cap=cv2.VideoCapture('sf3.mp4')
+
+
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE :  
+        point = [x, y]
+        print(point)
+       
+
+          
+    
+        
+
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', RGB)
+while True:
+    ret,frame=cap.read()
+    if not ret:
+       cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+       continue
+    frame=cv2.resize(frame,(1020,800))
+
+    cv2.imshow("RGB",frame)
+    if cv2.waitKey(100) & 0xFF == 27:
+        break
+cap.release()    
+cv2.destroyAllWindows()     
---
+++ test1.py
@@ -0,0 +1,21 @@
+import cv2
+import pickle
+import numpy as np
+import cvzone
+
+
+cap=cv2.VideoCapture('sf3.mp4')
+
+
+while True:
+      ret,frame=cap.read()
+      if not ret:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        continue
+      frame=cv2.resize(frame,(1020,800))
+      cv2.imshow("FRAME",frame)
+
+      if cv2.waitKey(90) & 0xFF == 27:
+         break
+cap.release()    
+cv2.destroyAllWindows()     
